a9f488ef20b025e599ae53fa43f3199677e353f0
elagutie <elagutie@bancolombia.com.co>
2021-10-21 07:47:42 -0500

    202110210747 - Create http request /getItemById

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,43 @@
 const { Router }    = require('express');
 const fetch         = require('node-fetch')
 const router        = Router();
+const urlBase       = 'https://api.mercadolibre.com/';
 
-router.get('/', async(req, res) => {
-    console.log(req)
-    const response = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=iphone&limit=4');
+function getQueryString( query ){
+    if( !query ){
+        return false 
+    }
+    let queryString = (JSON.stringify(query)); 
+    queryString = queryString.replace(/"/g, '');
+    queryString = queryString.replace(/{/g, '');
+    queryString = queryString.replace(/}/g, '');
+    queryString = queryString.replace(/,/g,'&');
+    queryString = queryString.replace(/:/g,'=');
+    return queryString;
+}
+
+router.get('/getItemsByQuery', async(req, res) => {
+    const queryString = getQueryString( req.query ); 
+
+    if ( !queryString){
+        return false;
+    }
+    const response = await fetch(`${urlBase}sites/MLA/search?${queryString}`);
     const users = await response.json();
     res.json(users);
 })
+
+router.get('/getItemById', async(req, res) => {
+    const id = req.query.id
+    if ( !id ){
+        return false;
+    }
+    const response = await fetch(`${urlBase}items/${id}`); //MLA1109059186
+    const users = await response.json();
+    console.log(users);
+    res.json(users);
+})
+
+// https://api.mercadolibre.com/items/MLA1109059186
 
 module.exports = router;
